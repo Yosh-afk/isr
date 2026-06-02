fb346aedf013b495b56cec724af4d4de48e6e724
yosh <yosgard.gonzalez.alp@cbtis258.edu.mx>
2026-06-02 16:06:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,18 @@
-daily: {
-  days: 1,
-  rows: [
-    { lower: 0.01, upper: 27.78, fixed: 0.00, rate: 1.92 },
-    { lower: 27.79, upper: 235.81, fixed: 0.53, rate: 6.40 },
-    { lower: 235.82, upper: 414.41, fixed: 13.85, rate: 10.88 },
-    { lower: 414.42, upper: 481.73, fixed: 33.28, rate: 16.00 },
-    { lower: 481.74, upper: 576.38, fixed: 44.05, rate: 17.92 },
-    { lower: 576.39, upper: 1163.25, fixed: 61.30, rate: 21.36 },
-    { lower: 1163.26, upper: 1833.44, fixed: 186.34, rate: 23.52 },
-    { lower: 1833.45, upper: 3500.35, fixed: 343.98, rate: 30.00 },
-    { lower: 3500.36, upper: 4667.12, fixed: 844.24, rate: 32.00 },
-    { lower: 4667.13, upper: 14001.38, fixed: 1217.16, rate: 34.00 },
-    { lower: 14001.39, upper: Infinity, fixed: 4387.90, rate: 35.00 }
-  ]
-},
+const payroll = {
+  date: document.querySelector("#payroll-date"),
+  period: document.querySelector("#payroll-period"),
+  gross: document.querySelector("#payroll-gross"),
+  tax: document.querySelector("#payroll-tax"),
+  subsidy: document.querySelector("#payroll-subsidy"),
+  net: document.querySelector("#payroll-net"),
 
-weekly: {
-  days: 7,
-  rows: [
-    { lower: 0.01, upper: 194.46, fixed: 0.00, rate: 1.92 },
-    { lower: 194.47, upper: 1655.08, fixed: 3.72, rate: 6.40 },
-    { lower: 1655.09, upper: 2907.47, fixed: 97.53, rate: 10.88 },
-    { lower: 2907.48, upper: 3379.42, fixed: 233.80, rate: 16.00 },
-    { lower: 3379.43, upper: 4043.52, fixed: 309.31, rate: 17.92 },
-    { lower: 4043.53, upper: 8160.35, fixed: 430.24, rate: 21.36 },
-    { lower: 8160.36, upper: 12862.31, fixed: 1309.88, rate: 23.52 },
-    { lower: 12862.32, upper: 24548.67, fixed: 2415.57, rate: 30.00 },
-    { lower: 24548.68, upper: 32731.55, fixed: 5919.47, rate: 32.00 },
-    { lower: 32731.56, upper: 98194.52, fixed: 8537.23, rate: 34.00 },
-    { lower: 98194.53, upper: Infinity, fixed: 30794.07, rate: 35.00 }
-  ]
-},
+  income: document.querySelector("#summary-income"),
+  deductions: document.querySelector("#summary-deductions"),
+  summaryNet: document.querySelector("#summary-net"),
+};
+
+const generatePayrollBtn =
+  document.querySelector("#generate-payroll");
+
+const printPayrollBtn =
+  document.querySelector("#print-payroll");
